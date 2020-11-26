bd8713c86f8642cc21b294ebebf63c7a89ea4a08
Jithin Roy <51138777+royjit@users.noreply.github.com>
2020-11-25 17:27:08 -0800

    test(auth): Added signedOut test for authorizationProvider

--- AmplifyPlugins/Auth/AWSCognitoAuthPluginTests/AuthorizationProviderTests/AuthorizationProviderSessionSignoutTests.swift
+++ AmplifyPlugins/Auth/AWSCognitoAuthPluginTests/AuthorizationProviderTests/AuthorizationProviderSessionSignoutTests.swift
@@ -11,21 +11,142 @@ import XCTest
 @testable import Amplify
 @testable import AWSCognitoAuthPlugin
 @testable import AWSMobileClient
+import AWSPluginsCore
 
-class AuthorizationProviderSessionSignoutTests: XCTestCase {
-
-    var authenticationProvider: AuthenticationProviderAdapter!
-    var mockAWSMobileClient: MockAWSMobileClient!
-    var plugin: AWSCognitoAuthPlugin!
+class AuthorizationProviderSessionSignoutTests: BaseAuthorizationProviderTest {
 
     override func setUp() {
-        mockAWSMobileClient = MockAWSMobileClient()
-        authenticationProvider = AuthenticationProviderAdapter(awsMobileClient: mockAWSMobileClient!)
-        plugin = AWSCognitoAuthPlugin()
-        plugin?.configure(authenticationProvider: authenticationProvider,
-                         authorizationProvider: MockAuthorizationProviderBehavior(),
-                         userService: MockAuthUserServiceBehavior(),
-                         deviceService: MockAuthDeviceServiceBehavior(),
-                         hubEventHandler: MockAuthHubEventBehavior())
+        super.setUp()
+        mockAWSMobileClient.mockCurrentUserState = .guest
+    }
+
+    func testSignoutSessionWithUnAuthAccess() {
+
+        let mockAWSCredentials = AWSCredentials(accessKey: "mockAccess",
+                                                secretKey: "mockSecret",
+                                                sessionKey: "mockSession",
+                                                expiration: Date())
+        mockAWSMobileClient.awsCredentialsMockResult = .success(mockAWSCredentials)
+        mockAWSMobileClient.getIdentityIdMockResult = AWSTask(result: "mockIdentityId")
+
+        let resultExpectation = expectation(description: "Should receive a result")
+        _ = plugin.fetchAuthSession(options: AuthFetchSessionRequest.Options()) { result in
+            defer {
+                resultExpectation.fulfill()
+            }
+            switch result {
+            case .success(let session):
+
+                XCTAssertFalse(session.isSignedIn)
+
+                let creds = try? (session as? AuthAWSCredentialsProvider)?.getAWSCredentials().get()
+                XCTAssertNotNil(creds?.accessKey)
+                XCTAssertNotNil(creds?.secretKey)
+
+                let identityId = try? (session as? AuthCognitoIdentityProvider)?.getIdentityId().get()
+                XCTAssertNotNil(identityId)
+
+                let tokensResult = (session as? AuthCognitoTokensProvider)?.getCognitoTokens()
+                guard case .failure(let error) = tokensResult,
+                      case .signedOut = error else {
+                    XCTFail("Should return signedOut error")
+                    return
+                }
+
+            case .failure(let error):
+                XCTFail("Received failure with error \(error)")
+            }
+        }
+        wait(for: [resultExpectation], timeout: apiTimeout)
+    }
+
+    func testInvalidCredentialsResponseInSignedOut() {
+
+        mockAWSMobileClient.awsCredentialsMockResult = nil
+        mockAWSMobileClient.getIdentityIdMockResult = AWSTask(result: "mockIdentityId")
+
+        let resultExpectation = expectation(description: "Should receive a result")
+        _ = plugin.fetchAuthSession(options: AuthFetchSessionRequest.Options()) { result in
+            defer {
+                resultExpectation.fulfill()
+            }
+            switch result {
+            case .success(let session):
+                XCTAssertFalse(session.isSignedIn)
+
+                XCTAssertFalse(session.isSignedIn)
+
+                let credentialsResult = (session as? AuthAWSCredentialsProvider)?.getAWSCredentials()
+                guard case .failure(let credentialsError) = credentialsResult,
+                      case .unknown = credentialsError else {
+                    XCTFail("Should return unknown error")
+                    return
+                }
+
+                let identityIdResult = (session as? AuthCognitoIdentityProvider)?.getIdentityId()
+                guard case .failure(let identityIdError) = identityIdResult,
+                      case .unknown = identityIdError else {
+                    XCTFail("Should return unknown error")
+                    return
+                }
+
+                let tokensResult = (session as? AuthCognitoTokensProvider)?.getCognitoTokens()
+                guard case .failure(let error) = tokensResult,
+                      case .signedOut = error else {
+                    XCTFail("Should return signedOut error")
+                    return
+                }
+            case .failure(let error):
+                XCTFail("Received failure with error \(error)")
+            }
+        }
+        wait(for: [resultExpectation], timeout: apiTimeout)
+    }
+
+    func testInvalidIdentityIdResponseInSignedOut() {
+
+        let mockAWSCredentials = AWSCredentials(accessKey: "mockAccess",
+                                                secretKey: "mockSecret",
+                                                sessionKey: "mockSession",
+                                                expiration: Date())
+        mockAWSMobileClient.awsCredentialsMockResult = .success(mockAWSCredentials)
+        mockAWSMobileClient.getIdentityIdMockResult = AWSTask(result: nil)
+
+        let resultExpectation = expectation(description: "Should receive a result")
+        _ = plugin.fetchAuthSession(options: AuthFetchSessionRequest.Options()) { result in
+            defer {
+                resultExpectation.fulfill()
+            }
+            switch result {
+            case .success(let session):
+                XCTAssertFalse(session.isSignedIn)
+
+                XCTAssertFalse(session.isSignedIn)
+
+                let credentialsResult = (session as? AuthAWSCredentialsProvider)?.getAWSCredentials()
+                guard case .failure(let credentialsError) = credentialsResult,
+                      case .unknown = credentialsError else {
+                    XCTFail("Should return unknown error")
+                    return
+                }
+
+                let identityIdResult = (session as? AuthCognitoIdentityProvider)?.getIdentityId()
+                guard case .failure(let identityIdError) = identityIdResult,
+                      case .unknown = identityIdError else {
+                    XCTFail("Should return unknown error")
+                    return
+                }
+
+                let tokensResult = (session as? AuthCognitoTokensProvider)?.getCognitoTokens()
+                guard case .failure(let error) = tokensResult,
+                      case .signedOut = error else {
+                    XCTFail("Should return signedOut error")
+                    return
+                }
+            case .failure(let error):
+                XCTFail("Received failure with error \(error)")
+            }
+        }
+        wait(for: [resultExpectation], timeout: apiTimeout)
     }
 }
